5676913865b768d8bde1da54ba41b49e26a9c2c6
Ryan Peden <ryan04@gmail.com>
2017-06-18 14:29:18 -0400

    added external css file and moved inline story group styles to css

--- frontend/components/StoryGroup/StoryGroup.jsx
+++ frontend/components/StoryGroup/StoryGroup.jsx
@@ -3,19 +3,18 @@ import Paper from "material-ui/Paper";
 import _ from "lodash";
 
 import Story from "../Story/Story.jsx";
-import styles from "../../utils/styles";
 
 const StoryGroup = ({storyGroup}) => {
     const sortedStories = _.orderBy(storyGroup.stories, ["points"], ["desc"]);
     return (
-        <Paper key={storyGroup.site} zDepth={2} style={styles.storyGroup}>
-          <div style={{ margin: "30px" }}>
+        <Paper key={storyGroup.site} zDepth={2} className="story-group-container">
+          <div className="story-group-inner">
             <div style={{ marginBottom: "20px", verticalAlign: "middle"}}>
-              <img className="story-logo" src={`/logos/${storyGroup.logo}`} />
-              <span className="story-group-title">{storyGroup.site}</span>
+              <img className="story-group-logo" src={`/logos/${storyGroup.logo}`} />
+              <span className="story-group-site-name">{storyGroup.site}</span>
             </div>
-            <div style={styles.story.header.points}>Points</div>
-            <div style={styles.story.header.title}>Title</div>
+            <div className="story-group-points">Points</div>
+            <div className="story-group-title">Title</div>
             <div style={{ marginTop: "10px" }}>
               {sortedStories.map((story) => <Story story={story} /> )}
             </div>
