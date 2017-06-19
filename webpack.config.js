2b835871a4e3121559a80f17860b61240b04dc16
Ryan Peden <ryan04@gmail.com>
2017-06-18 20:55:55 -0400

    changes to make app more mobile friendly

--- webpack.config.js
+++ webpack.config.js
@@ -22,20 +22,13 @@ module.exports = {
     }, { test: /\.json$/, loader: "json" } ]
   },
   resolve: {
-    /*alias: {
-      "react": "preact-compat",
-      "react-dom": "preact-compat",
-      "react-addons-transition-group": "preact-transition-group"
-    },*/
     extensions: [".js", ".jsx"],
     modules: [
-      //path.resolve('./client'),
       "node_modules"
     ]
   },
   plugins: [
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    //new BabiliPlugin(),
     new webpack.DefinePlugin({
       "process.env": {
         "NODE_ENV": JSON.stringify("production")
@@ -45,21 +38,13 @@ module.exports = {
       minimize: true,
       debug: false
     }),
-    /*new webpack.optimize.UglifyJsPlugin({
+    new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
       },
       output: {
         comments: false
       }
-    })*/
-    /*new ClosureCompilerPlugin({
-      compiler: {
-        "language_in": "ECMASCRIPT6",
-        "language_out": "ECMASCRIPT5_STRICT",
-        "compilation_level": "SIMPLE"
-      },
-      //eslint-disable-next-line
-      concurrency: 6,
-  })*/]
+    })
+  ]
 };
