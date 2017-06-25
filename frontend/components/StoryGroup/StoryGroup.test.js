3b41e73611fc14ce985c27d3c62154c56ed2bd2c
Ryan Peden <ryan04@gmail.com>
2017-06-25 16:49:03 -0400

    added story group tests

---
+++ frontend/components/StoryGroup/StoryGroup.test.js
@@ -0,0 +1,59 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {shallow} from "enzyme";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import theme from "../../utils/theme";
+
+import StoryGroup from "./StoryGroup.jsx";
+
+const testStories = [
+  {
+    title: "Test Title",
+    points: 50,
+    address: "http://nowhere.com"
+  },
+  {
+    title: "Test Two",
+    points: 55,
+    address: "http://nowhere2.com"
+  },
+  {
+    title: "Test Three",
+    points: 60,
+    address: "http://nowhere3.com"
+  }
+];
+
+const storyGroupData = {
+  stories: testStories,
+  site: "Test Site",
+  logo: "nothing.png"
+};
+
+test("Story group snapshot", () => {
+  const component = renderer.create(
+    <MuiThemeProvider muiTheme={theme}>
+      <StoryGroup storyGroup={storyGroupData} />
+    </MuiThemeProvider>
+  );
+  const storyGroup = component.toJSON();
+  expect(storyGroup).toMatchSnapshot();
+});
+
+test("Title displays correctly", () => {
+  const component = shallow(
+      <StoryGroup storyGroup={storyGroupData} />
+  );
+
+  const storyName = component.find(".story-group-site-name").text();
+  expect(storyName).toBe(storyGroupData.site);
+});
+
+test("Correct group logo is used", () => {
+  const component = shallow(
+      <StoryGroup storyGroup={storyGroupData} />
+  );
+
+  const groupLogoSrc = component.find(".story-group-logo").prop("src");
+  expect(groupLogoSrc).toMatch(storyGroupData.logo);
+});
