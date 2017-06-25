7fe069c90da15247a32de478212075d82f740d8d
Ryan Peden <ryan04@gmail.com>
2017-06-25 15:29:20 -0400

    added to story test and updated snapshot

--- frontend/components/Story/Story.test.js
+++ frontend/components/Story/Story.test.js
@@ -3,13 +3,13 @@ import Story from "./Story.jsx";
 import renderer from "react-test-renderer";
 import {shallow} from "enzyme";
 
-test("Story snapshot works", () => {
-  const testStory = {
-    title: "Test Title",
-    points: 50,
-    address: "http://nowhere.com"
-  };
+const testStory = {
+  title: "Test Title",
+  points: 50,
+  address: "http://nowhere.com"
+};
 
+test("Story snapshot works", () => {
   const component = renderer.create(
     <Story story={testStory} />
   );
@@ -18,5 +18,15 @@ test("Story snapshot works", () => {
 });
 
 test("Story renders as expected", () => {
+  const story = shallow(
+    <Story story={testStory} />
+  );
 
+  const storyText = story.text();
+  expect(storyText).toMatch(`${testStory.points} points`);
+  expect(storyText).toMatch(testStory.title);
+
+  const storyLink = story.find("a");
+  expect(storyLink.text()).toBe(testStory.title);
+  expect(storyLink.prop("href")).toBe(testStory.address);
 });
