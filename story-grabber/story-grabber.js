1d6c9bb28414b15ccaeebcfba6a39c6182889fe0
Ryan Peden <ryan04@gmail.com>
2018-05-04 11:13:17 -0400

    updated to only look at old reddit site

--- story-grabber/story-grabber.js
+++ story-grabber/story-grabber.js
@@ -1,6 +1,16 @@
 const _ = require("lodash");
 const parsers = require("./parser-definitions.js");
-const cheerio = require("./cheerio-bundle.js");
+const cheerio = require("cheerio");
+const winston = require("winston");
+const fs = require('fs');
+
+const consoleLogger = winston.createLogger({
+  level: "info",
+  transports: [
+    new winston.transports.Console()
+  ]
+});
+
 require("isomorphic-fetch");
 
 const hackerNewsParser = (dom, promise, minPoints, logo) => {
@@ -43,7 +53,7 @@ const hackerNewsParser = (dom, promise, minPoints, logo) => {
   promise.resolve({site: "Hacker News", stories: parsedStories, logo});
 };
 
-const makeHNParser = (minimumPoints, logo) => {
+const makeHNParser = (minimumPoints, logo, logger) => {
   return new Promise((resolve, reject) => {
     fetch("https://news.ycombinator.com")
       .then((response) => {
@@ -51,73 +61,107 @@ const makeHNParser = (minimumPoints, logo) => {
       })
       .then((body) => {
         const doc = cheerio.load(body);
-        hackerNewsParser({err: null, document: doc}, {resolve, reject}, minimumPoints, logo);
+        hackerNewsParser({err: null, document: doc},
+                        {resolve, reject}, minimumPoints, logo, logger);
       })
-      .catch((error) => reject(error));
+      .catch((error) => {
+        logger.log({
+          level: "error",
+          message: error
+        });
+        reject(error);
+      });
   });
 };
 
-const redditParser = (dom, promise, title, minPoints, logo) => {
-  if (dom.err) { promise.reject([]); }
+const redditParser = (dom, promise, title, minPoints, logo, logger) => {
+  if (dom.err) {
+    logger.log({
+      level: "error",
+      message: dom.err
+    });
+    promise.reject([]);
+    return;
+  }
+
   const $ = dom.document;//jQuery(dom.window);
   // extract the stories
   const stories = $(".link");
   const parsedStories = [];
-
+  if (stories.length === 0) {
+    logger.log({
+      level: "error",
+      message: "got zero stories"
+    });
+  }
     // extract what we need from each story
   _.each(stories, (story) => {
-    //extract title
-    const titleAnchor = $(story).find(".title").find(".title");
-    const titleText = titleAnchor.text();
-    // extract URL
-    let address = titleAnchor.attr("href") || "";
-    if (!address.startsWith("http")) {
-      address = `https://www.reddit.com${address}`;
-    }
-    // extract score/points
-    const points = parseInt($(story).find(".likes").text()) || 0;
-    // extract comments count and link
-    const commentsAnchor = $(story).find("a[data-event-action=comments]");
-    const commentsCount = parseInt(commentsAnchor.text());
-    let commentsAddress = commentsAnchor.attr("href");
-    if (!commentsAddress.startsWith("http")) {
-      commentsAddress = `https://www.reddit.com${address}`;
-    }
+    try {
+      //extract title
+      const titleAnchor = $(story).find(".title").find(".title");
+      const titleText = titleAnchor.text();
+      // extract URL
+      let address = titleAnchor.attr("href") || "";
+      if (!address.startsWith("http")) {
+        address = `https://www.reddit.com${address}`;
+      }
+      // extract score/points
+      const points = parseInt($(story).find(".likes").text()) || 0;
 
-    if (points >= minPoints) {
-      parsedStories.push({
-        title: titleText,
-        address,
-        points,
-        commentsAddress,
-        commentsCount
+      // extract comments count and link
+      const commentsAnchor = $(story).find("a[data-event-action=comments]");
+      const commentsCount = parseInt(commentsAnchor.text());
+      let commentsAddress = commentsAnchor.attr("href");
+      if (!commentsAddress.startsWith("http")) {
+        commentsAddress = `https://www.reddit.com${address}`;
+      }
+
+      if (points >= minPoints) {
+        parsedStories.push({
+          title: titleText,
+          address,
+          points,
+          commentsAddress,
+          commentsCount
+        });
+      }
+    } catch (e) {
+      logger.log({
+        level: "error",
+        message: e
       });
     }
   });
   promise.resolve({ site: title, stories: parsedStories, logo});
 };
 
-const makeRedditParser = (subReddit, title, minimumPoints, logo) => {
+const makeRedditParser = (subReddit, title, minimumPoints, logo, logger) => {
   return new Promise((resolve, reject) => {
-    fetch(`https://www.reddit.com/r/${subReddit}`)
+    fetch(`https://old.reddit.com/r/${subReddit}`)
       .then((response) => response.text())
       .then((body) => {
         const doc = cheerio.load(body);
         redditParser({ err: null, document: doc},
                      { resolve, reject },
-                     title, minimumPoints, logo);
+                     title, minimumPoints, logo, logger);
       })
-      .catch((error) => reject(error));
+      .catch((error) => {
+        logger.log({
+          level: "error",
+          message: error
+        });
+        reject(error);
+      });
   });
 };
 
 const createParser = (parserInfo) => {
   switch (parserInfo.type) {
   case "HackerNews":
-    return makeHNParser(parserInfo.minimumScore, parserInfo.logo);
+    return makeHNParser(parserInfo.minimumScore, parserInfo.logo, consoleLogger);
   case "Reddit":
     return makeRedditParser(parserInfo.subReddit, parserInfo.title,
-                            parserInfo.minimumScore, parserInfo.logo);
+                            parserInfo.minimumScore, parserInfo.logo, consoleLogger);
   default:
     return Promise.resolve();
   }
